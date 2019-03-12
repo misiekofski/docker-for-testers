57c82e91e09c138c5cb2afb4730381eed35ac278
Michal Dobrzycki <m.dobrzycki@avanade.com>
2019-03-12 09:18:47 +0100

    Fixing collation for dbs

--- 2_running_with_dockerfile/sqls/create.sql
+++ 2_running_with_dockerfile/sqls/create.sql
@@ -1,4 +1,8 @@
-CREATE TABLE pracoholicy (
+CREATE DATABASE pracoholicy CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+USE pracoholicy;
+
+CREATE TABLE pracholicy.pracoholicy (
 alkoholikID int AUTO_INCREMENT PRIMARY KEY,
 first_name varchar(25),
 last_name  varchar(25),

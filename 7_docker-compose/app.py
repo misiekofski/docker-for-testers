c63e38633d327943f9c7a9955ffeda4c0ae67728
Michal Dobrzycki <dobrzycki@gmail.com>
2019-06-25 00:18:59 +0200

    Changed a bit flask app

--- 7_docker-compose/app.py
+++ 7_docker-compose/app.py
@@ -23,7 +23,7 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'You are awesome! You did shots {} times.\n'.format(count)
+    return 'You are awesome! You drank shots {} times.\n'.format(count)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
